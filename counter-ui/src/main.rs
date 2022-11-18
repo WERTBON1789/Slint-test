1b6bf54e749b26e908cd0676fa565687a23ed830
WERTBON1789 <50809022+WERTBON1789@users.noreply.github.com>
2022-11-18 02:11:57 +0100

    Added counter-ui, a simple example for shared state mutated with two buttons

---
+++ counter-ui/src/main.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+
+use std::{rc::Rc, borrow::{BorrowMut, Borrow}, cell::{RefCell, Cell}, ops::AddAssign};
+
+
+
+
+
+slint::include_modules!();
+fn main() {
+    let counter = Counter::new();
+
+    let mut counter_num = Rc::from(Cell::from(0i32));
+
+    let counter_num_ref = Rc::clone(&counter_num);
+
+    let inc_weak = counter.as_weak();
+
+    let dec_weak = counter.as_weak();
+
+    counter.on_inc(move || {
+        counter_num.set(counter_num.get()+1);
+        inc_weak.unwrap().global::<Label>().set_label(slint::SharedString::from(format!("{}", counter_num.get())));
+    });
+
+    counter.on_dec(move || {
+        counter_num_ref.set(counter_num_ref.get()-1);
+        dec_weak.unwrap().global::<Label>().set_label(slint::SharedString::from(format!("{}", counter_num_ref.get())));
+    });
+
+    counter.run();
+}
